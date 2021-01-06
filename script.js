d44e2365e8b66602a91d5c7eac0f75ad6e06d830
Brian Janssen <brian-janssen@hotmail.com>
2021-01-06 12:04:00 +0100

    Make the time 0 padded

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ function displayTime() {
     var today = new Date();
     var hours = today.getHours();
     var minutes = today.getMinutes();
+    if (hours < 10) hours = `0${hours}`
+    if (minutes < 10) minutes = `0${minutes}`
     document.getElementById("time").innerHTML = `${hours}:${minutes}`;
     determineGreet(hours);
 }
